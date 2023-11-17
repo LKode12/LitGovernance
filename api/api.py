a02fe47667e4457a25b47482ed469f37e5c66e07
Magalaweng Clive Mmakola <mmakolaclivem@gmail.com>
2023-11-17 17:24:50 +0200

    Add api sql query skeleton

--- api/api.py
+++ api/api.py
@@ -3,8 +3,28 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
-class API:
 
-    def __inti__(self):
 
+class questionaireAPI:
+
+    def __inti__(self, jsonQuestinaireRequest):
+
+        # Request from the assessment q
+        self.jsonQuestinaireRequest = jsonQuestinaireRequest
+
+        # The request body will have the company key and director Id
+        # This will be used to update the relevant table
+        self.data = request.data
+
+    @app.route("/assessment", methods=[POST])
+    def queryDb():
+        
+        # This should generate an sql query to update relevant table
+
+        pass
+
+    def queryResponse():
+
+        # If the sql query was successful return response and direct it to homepage
+        # so we pop the board member from list of directors 
         
